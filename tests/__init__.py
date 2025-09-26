8bda3a94a737ff663640ee135c95c70d3c324399
Sandra-Cai <109099733+Sandra-Cai@users.noreply.github.com>
2025-09-26 19:59:15 -0400

    update

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,18 @@
-# Tests package
+"""
+Advanced Testing Framework
+Comprehensive testing suite for Opinion Market platform
+"""
+
+from .conftest import *
+from .test_utils import *
+from .test_fixtures import *
+
+__all__ = [
+    "pytest_configure",
+    "pytest_collection_modifyitems",
+    "create_test_client",
+    "create_test_database",
+    "create_test_cache",
+    "TestDataFactory",
+    "MockExternalServices"
+]

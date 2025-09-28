a4272ad3871206b5ff302f60cd650d492c3bd975
Sandra-Cai <109099733+Sandra-Cai@users.noreply.github.com>
2025-09-27 23:25:48 -0400

    update

---
+++ app/analytics/__init__.py
@@ -0,0 +1,20 @@
+"""
+Advanced Analytics Engine
+Provides machine learning, predictive analytics, and business intelligence
+"""
+
+from .analytics_engine import AnalyticsEngine
+from .ml_predictor import MLPredictor
+from .trend_analyzer import TrendAnalyzer
+from .user_behavior_analyzer import UserBehaviorAnalyzer
+from .market_analyzer import MarketAnalyzer
+from .reporting_engine import ReportingEngine
+
+__all__ = [
+    "AnalyticsEngine",
+    "MLPredictor",
+    "TrendAnalyzer",
+    "UserBehaviorAnalyzer",
+    "MarketAnalyzer",
+    "ReportingEngine"
+]
